39f24ccc3941139ff7df32f12443c23dffcc9111
Yastreb-I <122534234+Yastreb-I@users.noreply.github.com>
2023-06-09 23:26:18 +0800

    Add files via upload

---
+++ news/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    userAuthor = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.IntegerField(default=0)
+
+    def update_rating(self):
+        # суммарный рейтинг каждой статьи автора
+        rating_post = self.post_set.all().aggregate(sum_rating=Sum("ratingPost"))
+        col_post = 0
+        col_post += rating_post.get("sum_rating")
+
+        # суммарный рейтинг всех комментариев автора
+        rating_comment = self.userAuthor.comment_set.all().aggregate(sum_rating=Sum("ratingComment"))
+        col_com = 0
+        col_com += rating_comment.get("sum_rating")
+
+        # суммарный рейтинг всех комментариев к статьям автора.
+        others_rating = Comment.objects.filter(commentPost__author=self).exclude(
+            commentUser=self.userAuthor).aggregate(Sum('ratingComment'))
+        col_oth = 0
+        if others_rating.get('ratingComment__sum') == type("int"):
+            col_oth += others_rating.get('ratingComment__sum')
+
+        # Считаем рейтинг автора и сохраняем его
+        self.ratingAuthor = col_com + col_oth + col_post * 3
+        self.save()
+
+
+class Category(models.Model):
+    nameNewsCategories = models.CharField(max_length=96, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    NEWS = 'NS'
+    ARTICLE = 'AR'
+    CHOICES_TYPE_POST = [
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    ]
+    typePost = models.CharField(max_length=2, choices=CHOICES_TYPE_POST, default=NEWS)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    categoriesPost = models.ManyToManyField(Category, through="PostCategory")
+    headPost = models.CharField(max_length=192)
+    textPost = models.TextField()
+    ratingPost = models.IntegerField(default=0)
+
+    def like(self):
+        self.ratingPost += 1
+        self.save()
+
+    def dislike(self):
+        self.ratingPost -= 1
+        self.save()
+
+    # Предварительный просмотр поста
+    def preview(self):
+        return self.textPost[0:123] + "..."
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    textComment = models.TextField()
+    ratingComment = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.commentUser.username
+
+    def like(self):
+        self.ratingComment += 1
+        self.save()
+
+    def dislike(self):
+        self.ratingComment -= 1
+        self.save()
+
+# Create your models here.
